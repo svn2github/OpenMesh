07863005826fc1bef02be1ae90cb7fb7d4dad7fd
moebius <moebius@fdac6126-5c0c-442c-9429-916003d36597>
2009-02-19 07:14:50 +0000

    Explicit cast to avoid warning
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@82 fdac6126-5c0c-442c-9429-916003d36597

--- trunk/Tools/Utils/getopt.c
+++ trunk/Tools/Utils/getopt.c
@@ -74,7 +74,7 @@ getopt(int nargc, char * const *nargv, const char *ostr)
 		}
 	}					/* option letter okay? */
 	if ((optopt = (int)*place++) == (int)':' ||
-	    !(oli = strchr(ostr, optopt))) {
+	    !(oli = (char *)strchr(ostr, optopt))) {
 		/*
 		 * if the user didn't specify '-' as an option,
 		 * assume it means -1.
