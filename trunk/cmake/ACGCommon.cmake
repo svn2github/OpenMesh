9fe9151674b0b80a99aff47376272f552eed734c
moebius <moebius@fdac6126-5c0c-442c-9429-916003d36597>
2009-06-23 18:52:32 +0000

    Added sonames for OpenMesh libs
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@173 fdac6126-5c0c-442c-9429-916003d36597

--- trunk/cmake/ACGCommon.cmake
+++ trunk/cmake/ACGCommon.cmake
@@ -359,12 +359,6 @@ function (acg_add_library _target _libtype)
 
   add_library (${_target} ${_type} ${ARGN} )
 
-#  acg_get_version()
-#
-#  set_target_properties (${_target} PROPERTIES VERSION    ${VERSION-MAJOR}.${VERSION-MINOR} 
-#                                               SOVERSION  ${VERSION-MAJOR}.${VERSION-MINOR} )
-#
-
   # set common target properties defined in common.cmake
   acg_set_target_props (${_target})
 
