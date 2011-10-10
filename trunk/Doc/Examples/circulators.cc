dd43781cf6c91264ac0eb3a4b3e6a32c8e0e9112
moebius <moebius@fdac6126-5c0c-442c-9429-916003d36597>
2011-10-10 12:52:28 +0000

    Document behaviour of circulators on deleted elements
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@426 fdac6126-5c0c-442c-9429-916003d36597

--- trunk/Doc/Examples/circulators.cc
+++ trunk/Doc/Examples/circulators.cc
@@ -1,7 +1,7 @@
 MyMesh mesh;
 
 // (linearly) iterate over all vertices
-for (MyMesh::VertexIter v_it=mesh.vertices_begin(); v_it!=mesh.vertices_end(); ++v_it)
+for (MyMesh::VertexIter v_it=mesh.vertices_sbegin(); v_it!=mesh.vertices_end(); ++v_it)
 {
   // circulate around the current vertex
   for (MyMesh::VertexVertexIter vv_it=mesh.vv_iter(v_it.handle()); vv_it; ++vv_it)
