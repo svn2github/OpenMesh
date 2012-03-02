c5efd7fa994b977f8190be2f955489a45017c9ac
moebius <moebius@fdac6126-5c0c-442c-9429-916003d36597>
2012-03-02 16:54:35 +0000

    More Xcode 4.3 compatibility fixes. closes #464
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@553 fdac6126-5c0c-442c-9429-916003d36597

--- trunk/src/OpenMesh/Core/Mesh/TriMeshT.hh
+++ trunk/src/OpenMesh/Core/Mesh/TriMeshT.hh
@@ -179,7 +179,7 @@ public:
   /// Edge split (= 2-to-4 split)
   /// Do not call PolyMeshT function below as this does the wrong operation
   inline void split(EdgeHandle _eh, const Point& _p)
-  { Kernel::split(_eh, add_vertex(_p)); }
+  { Kernel::split(_eh, this->add_vertex(_p)); }
 
   inline void split(EdgeHandle _eh, VertexHandle _vh)
   { Kernel::split(_eh, _vh); }
