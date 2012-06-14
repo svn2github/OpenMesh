bd3b4ec2a3d844c150a91ff4a7db7e5e613ee91b
moebius <moebius@fdac6126-5c0c-442c-9429-916003d36597>
2012-06-14 07:25:27 +0000

    Created 2.2 tag
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@589 fdac6126-5c0c-442c-9429-916003d36597

---
+++ tags/trunk/src/Unittests/unittests.cc
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include "unittests_common.hh"
+#include "unittests_property.hh"
+#include "unittests_loading.hh"
+#include "unittests_trimesh_iterators.hh"
+#include "unittests_trimesh_collapse.hh"
+#include "unittests_trimesh_circulators.hh"
+#include "unittests_decimater.hh"
+#include "unittests_trimesh_normal_calculations.hh"
+#include "unittests_trimesh_others.hh"
+
+int main(int _argc, char** _argv) {
+
+    testing::InitGoogleTest(&_argc, _argv);
+    return RUN_ALL_TESTS();
+}
