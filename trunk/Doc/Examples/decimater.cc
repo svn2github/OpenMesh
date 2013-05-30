40897d3611fb2487396cae328ad53bad18570192
lim <lim@fdac6126-5c0c-442c-9429-916003d36597>
2013-05-30 15:22:35 +0000

    adjusted the documation for the decimation tutorial so that the priority module is correctly initialized
    refs #1580
    
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@828 fdac6126-5c0c-442c-9429-916003d36597

--- trunk/Doc/Examples/decimater.cc
+++ trunk/Doc/Examples/decimater.cc
@@ -23,6 +23,13 @@ decimater.add( hModQuadric ); // register module at the decimater
 std::cout << decimater.module( hModQuadric ).name() << std::endl;
                               // the way to access the module 
 
+/*
+ * since we need exactly one priority module (non-binary)
+ * we have to call set_binary(false) for our priority module
+ * in the case of HModQuadric, unset_max_err() calls set_binary(false) internally
+ */
+decimater.module( hModQuadric ).unset_max_err();
+
 decimater.initialize();       // let the decimater initialize the mesh and the
                               // modules
 
