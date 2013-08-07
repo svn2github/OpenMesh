ad235279dc345009cb4dbcee0e732311dad62874
moeller <moeller@fdac6126-5c0c-442c-9429-916003d36597>
2013-08-07 15:23:04 +0000

    gcc version check based on compiler defines (fix for mingw)
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@913 fdac6126-5c0c-442c-9429-916003d36597

--- trunk/src/OpenMesh/Core/System/config.h
+++ trunk/src/OpenMesh/Core/System/config.h
@@ -80,8 +80,7 @@
 #if defined(_MSC_VER)
 #  define DEPRECATED(msg) __declspec(deprecated(msg))
 #elif defined(__GNUC__)
-#  include <features.h>
-#  if __GNUC_PREREQ(4,5)
+#  if (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__) >= 40500 /* Test for GCC >= 4.5.0 */
 #    define DEPRECATED(msg) __attribute__ ((deprecated(msg)))
 #  else
 #    define DEPRECATED(msg) __attribute__ ((deprecated))
