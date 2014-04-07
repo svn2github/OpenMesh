62d1ada33a9c69cc21e6bbb3e51a1535af157163
adielen <adielen@fdac6126-5c0c-442c-9429-916003d36597>
2014-04-07 15:31:40 +0000

    added intial unit tests for vertex related iterator and circulators
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@1007 fdac6126-5c0c-442c-9429-916003d36597

--- branches/OpenMesh-Python/src/Python/UnitTests/testBindings.py
+++ branches/OpenMesh-Python/src/Python/UnitTests/testBindings.py
@@ -3,17 +3,30 @@ import openmesh
 
 class TestIterators(unittest.TestCase):
 
-    def setUp(self):
-        self.mesh = openmesh.TriMesh()
-        vh0 = self.mesh.add_vertex( openmesh.Vec3d(0, 0, 0) )
-        vh1 = self.mesh.add_vertex( openmesh.Vec3d(1, 0, 0) )
-        vh2 = self.mesh.add_vertex( openmesh.Vec3d(0, 1, 0) )
-        self.mesh.add_face(vh0, vh1, vh2)
+        def setUp(self):
+                self.mesh = openmesh.TriMesh()
+                self.vh0 = self.mesh.add_vertex( openmesh.Vec3d(0, 0, 0) )
+                self.vh1 = self.mesh.add_vertex( openmesh.Vec3d(1, 0, 0) )
+                self.vh2 = self.mesh.add_vertex( openmesh.Vec3d(0, 1, 0) )
+                self.fh0 = self.mesh.add_face(self.vh0, self.vh1, self.vh2)
 
-    def test_vertex_iterator(self):
-        for v, i in zip(self.mesh.vertices, range(3)):
-            self.assertEqual(v.idx(), i)
+        def test_vertex_iterator(self):
+                for v, i in zip(self.mesh.vertices, range(3)):
+                        self.assertEqual(v.idx(), i)
+
+        def test_vertex_vertex_iterator(self):
+                for v, i in zip(self.mesh.vv(self.vh0), range(2, 1)):
+                        self.assertEqual(v.idx(), i)
+
+        def test_vertex_face_iterator(self):
+                for f in self.mesh.vf(self.vh0):
+                        self.assertEqual(f.idx(), self.fh0.idx())
+
+        def test_face_vertex_iterator(self):
+                for v, i in zip(self.mesh.fv(self.fh0), range(0, 2)):
+                        self.assertEqual(v.idx(), i)
+
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestIterators)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+        suite = unittest.TestLoader().loadTestsFromTestCase(TestIterators)
+        unittest.TextTestRunner(verbosity=2).run(suite)
