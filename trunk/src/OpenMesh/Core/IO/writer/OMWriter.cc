1ff8a60a6dcf23b6e1c7617954e9ab6bbe1b2710
moebius <moebius@fdac6126-5c0c-442c-9429-916003d36597>
2014-05-09 13:11:11 +0000

    Silence cppcheck for now
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@1055 fdac6126-5c0c-442c-9429-916003d36597

--- trunk/src/OpenMesh/Core/IO/writer/OMWriter.cc
+++ trunk/src/OpenMesh/Core/IO/writer/OMWriter.cc
@@ -423,7 +423,7 @@ bool _OMWriter_::write_binary(std::ostream& _os, BaseExporter& _be,
 				       OMFormat::Chunk::Entity_Mesh, swap );
   }
 
- //std::clog << "#bytes written: " << bytes << std::endl;
+  std::clog << "#bytes written: " << bytes << std::endl;
 
   return true;
 }
