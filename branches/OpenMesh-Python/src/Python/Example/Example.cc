98695110938ca174452219a27fba5e034b990d05
adielen <adielen@fdac6126-5c0c-442c-9429-916003d36597>
2014-09-12 07:44:44 +0000

    updated example
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@1161 fdac6126-5c0c-442c-9429-916003d36597

--- branches/OpenMesh-Python/src/Python/Example/Example.cc
+++ branches/OpenMesh-Python/src/Python/Example/Example.cc
@@ -4,7 +4,19 @@ char const * greet() {
 	return "hello, world";
 }
 
+struct World {
+	void set(std::string msg) { this->msg = msg; }
+	std::string greet() { return msg; }
+	std::string msg;
+};
+
 BOOST_PYTHON_MODULE(example) {
 	using namespace boost::python;
+	
 	def("greet", greet);
+	
+	class_<World>("World")
+		.def("greet", &World::greet)
+		.def("set", &World::set)
+	;
 }
