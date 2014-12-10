017b926188a28c430f273fd9e9ecd77ca39f3c16
moebius <moebius@fdac6126-5c0c-442c-9429-916003d36597>
2014-12-10 10:01:40 +0000

    Fixed clang warning
    git-svn-id: http://openmesh.org/svnrepo/OpenMesh@1185 fdac6126-5c0c-442c-9429-916003d36597

--- trunk/src/Unittests/unittests_vdpm.cc
+++ trunk/src/Unittests/unittests_vdpm.cc
@@ -227,10 +227,10 @@ TEST_F(OpenMeshVDPM, ReadPM)
 {
     LoadInfo info = open_progresult_mesh("cube1.pm");
 
-    EXPECT_EQ(7526, info.mesh.n_vertices()) << "Vertices differ";
-    EXPECT_EQ(6, info.mesh.n_edges()) << "Edges differ";
-    EXPECT_EQ(4, info.mesh.n_faces()) << "Faces differ";
-    EXPECT_EQ(7522, info.n_details) << "Details differ";
+    EXPECT_EQ(7526u, info.mesh.n_vertices()) << "Vertices differ";
+    EXPECT_EQ(6u, info.mesh.n_edges()) << "Edges differ";
+    EXPECT_EQ(4u, info.mesh.n_faces()) << "Faces differ";
+    EXPECT_EQ(7522u, info.n_details) << "Details differ";
 }
 
 /*
